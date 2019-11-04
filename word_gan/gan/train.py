986949e29e7b97ce7281a833293079162f2b7080
Andrey Vasnetsov <vasnetsov93@gmail.com>
2019-11-05 00:22:29 +0300

    disable logging

--- word_gan/gan/train.py
+++ word_gan/gan/train.py
@@ -41,7 +41,7 @@ def get_model(vocab, device) -> Tuple[Generator, Discriminator]:
         namespace='target'
     )
 
-    on_disk = True
+    on_disk = False
 
     token_fasttext_embedding: TokenEmbedder = load_fasttext(
         model_path=token_fasttext_path,
